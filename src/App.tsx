bf8ce5b36eb8c193aa30edbbe362f4d5edc8c4fa
yak <its.yak@gmail.com>
2022-04-06 20:15:01 +0300

    base

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [number, setNumber] = useState("");
+  const [isValid, setIsValid] = useState(false);
+
+  const link = buildLink();
+
+  function buildLink() {
+    let n = number.trim();
+    n = n.replaceAll("-", "");
+    return `https://wa.me/${n}`;
+  }
+  function validate(number: string) {
+    if (number == null) {
+      return false;
+    }
+    number = number.trim();
+
+    if (number === "") {
+      return false;
+    }
+
+    // const regex=/[0-9]{3}-[0-9]{3}-[0-9]{4}/;
+    const regex = /^[0-9]+[0-9,-]*$/;
+    return regex.test(number);
+  }
+  function onChange(e: ChangeEvent<HTMLInputElement>) {
+    //e.preventDefault();
+    setNumber(e.target.value);
+    setIsValid(validate(e.target.value));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div>
+        <label htmlFor="phone">Enter your phone number:</label>
+
+        <input
+          type="tel"
+          id="phone"
+          name="phone"
+          onChange={onChange}
+          value={number}
+          required
+        ></input>
+      </div>
+      <div>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          style={{
+            pointerEvents: isValid ? "inherit" : "none",
+            textDecoration: isValid ? "none" : "line-through",
+          }}
+          href={link}
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          {link}
         </a>
-      </header>
+      </div>
     </div>
   );
 }
